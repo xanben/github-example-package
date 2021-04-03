c320f9f5d98ebf569837a093d6ff75ffd78e39ee
alexandru.bencheci <alexandru.bencheci@vodafone.com>
2021-04-03 17:48:08 +0300

    update to use my username

--- src/main/java/unitTesting/Testing.java
+++ src/main/java/unitTesting/Testing.java
@@ -1,10 +1,5 @@
 package unitTesting;
 
-/**
- * @author Manjunath-PC
- * @created 18/08/2020
- * @project github-example-package
- */
 public class Testing {
 
     public static void display(String param){
